a491c0c8f97ded9fee8c185859557728ed563441
Travis Chen <travis@scopely.com>
2015-12-12 15:56:14 -0800

    Boost

---
+++ Assets/Scripts/Controllers/BoostController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoostController : ObjectController {
+
+	private tk2dSprite sprite; 
+	private BoxCollider boxCollider;
+
+	private const float BOOST_FORCE = 2000.0f;
+
+	// Use this for initialization
+	override public void Start () {
+	
+		base.Start();
+
+		sprite = GetComponent<tk2dSprite>();
+		boxCollider = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	override public void FixedUpdate () {
+
+		base.FixedUpdate();
+
+		if( gameController.IsRed() && ObjectIsBlue() )
+		{
+			sprite.color = UIManager.Instance.transparentColor;
+			boxCollider.enabled = false;
+		}
+		else if( gameController.IsBlue() && ObjectIsRed() )
+		{
+			sprite.color = UIManager.Instance.transparentColor;
+			boxCollider.enabled = false;
+		}
+		else
+		{
+			sprite.color = Color.white;
+			boxCollider.enabled = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if( other.tag == "Ball" )
+		{
+			BallController ball = other.gameObject.GetComponent<BallController>();
+			if( ball )
+			{
+				Rigidbody rigid = ball.GetComponent<Rigidbody>();
+				rigid.AddForce(Vector3.up * BOOST_FORCE);
+			}
+		}
+	}
+}
