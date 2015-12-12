ae4d615be032ffb1f424cf935be00ac397ea2896
Travis Chen <travis@scopely.com>
2015-12-12 15:24:52 -0800

    Update

---
+++ Assets/Scripts/Controllers/BoxController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxController : ObjectController {
+
+	private tk2dSprite sprite; 
+	private BoxCollider boxCollider;
+
+	// Use this for initialization
+	override public void Start () {
+	
+		base.Start();
+
+		sprite = GetComponent<tk2dSprite>();
+		boxCollider = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	override public void FixedUpdate () {
+
+		base.FixedUpdate();
+
+		if( gameController.IsRed() && ObjectIsBlue() )
+		{
+			sprite.color = UIManager.Instance.transparentColor;
+			boxCollider.enabled = false;
+		}
+		else if( gameController.IsBlue() && ObjectIsRed() )
+		{
+			sprite.color = UIManager.Instance.transparentColor;
+			boxCollider.enabled = false;
+		}
+		else
+		{
+			sprite.color = Color.white;
+			boxCollider.enabled = true;
+		}
+	}
+}
