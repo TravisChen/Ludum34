0fd7334eb464834527a375d2c19fd5a815ffe45c
Travis Chen <travis@scopely.com>
2015-12-13 11:59:15 -0800

    Portal

--- Assets/Scripts/Controllers/PortalEntranceController.cs
+++ Assets/Scripts/Controllers/PortalEntranceController.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class PortalEntranceController : ObjectController {
 
+	public ParticleSystem particle;
+
 	private tk2dSprite sprite; 
 	private SphereCollider sphereCollider;
+
+	private bool particleOn = false;
 
 	// Use this for initialization
 	override public void Start () {
@@ -23,16 +27,25 @@ public class PortalEntranceController : ObjectController {
 		if( gameController.IsRed() && ObjectIsBlue() )
 		{
 			sprite.color = RefManager.Instance.transparentColor;
+			particle.Stop();
+			particleOn = false;
 			sphereCollider.enabled = false;
 		}
 		else if( gameController.IsBlue() && ObjectIsRed() )
 		{
 			sprite.color = RefManager.Instance.transparentColor;
+			particle.Stop();
+			particleOn = false;
 			sphereCollider.enabled = false;
 		}
 		else
 		{
 			sprite.color = Color.white;
+			if( !particleOn )
+			{
+				particle.Play();
+				particleOn = true;
+			}
 			sphereCollider.enabled = true;
 		}
 	}
